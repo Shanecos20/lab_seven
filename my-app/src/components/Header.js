114b047951b887d91e8089888b503c62123c104f
unknown <g00391599@atu.ie>
2024-10-01 09:35:05 +0000

    Ex 4

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => {
-    return <h1>My Header in another component</h1>;
-};
+    return <h1 className="text-center text-primary">My Header in another component</h1>;
+  };
 
 export default Header;
