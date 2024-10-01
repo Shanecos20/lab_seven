30bfcfb69cbbb731fa514525d90aff4e393579a8
unknown <g00391599@atu.ie>
2024-10-01 09:39:27 +0000

    Ex 5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
+// import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <Content />
-      <Footer />
-    </div>
+    <Router>
+    <NavigationBar />
+    <Routes>
+      <Route path="/home" element={<Content />} />
+      <Route path="/read" element={<h1 className="text-center text-primary">Read Component</h1>} />
+      <Route path="/create" element={<h1 className="text-center text-primary">Create Component</h1>} />
+    </Routes>
+    <Footer />
+  </Router>
   );
 }
 
--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-    return <h3>My Footer in another component</h3>;
+    return <h3 className="text-center text-primary">My Footer in another component</h3>;
   };
   
 export default Footer;
