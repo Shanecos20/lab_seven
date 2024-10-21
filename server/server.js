39b4589ea1d35b980f506f153c4605d4d781a246
unknown <g00391599@atu.ie>
2024-10-21 14:10:01 +0000

    Comments

--- server/server.js
+++ server/server.js
@@ -1,29 +1,34 @@
-const express = require('express');
-const path = require('path'); // Importing the 'path' module
-const app = express();
-const port = 3000;
+const express = require('express'); // Importing the 'express' framework
+const path = require('path'); // Importing the 'path' module to handle file paths
+const app = express(); // Creating an instance of the express app
+const port = 3000; // Setting the port number to 3000
 
+// Default route that responds with a welcome message
 app.get('/', (req, res) => {
     res.send('Welcome to data rep and querying!!');
 });
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
+// Error-handling middleware, logs the error and sends a 500 status with a message
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
 });
 
+// Dynamic route that accepts two parameters (name and surname) and responds with a personalized message
 app.get('/hello/:name/:surname', (req, res) => {
-    const name = req.params.name;
-    const surname = req.params.surname;
-    res.send(`Hello ${name} ${surname}`);
+    const name = req.params.name;          // Extract 'name' from the URL parameters
+    const surname = req.params.surname;    // Extract 'surname' from the URL parameters
+    res.send(`Hello ${name} ${surname}`);  // Respond with a greeting message
 });
 
+// Route that responds with a JSON object of movies data
 app.get('/api/movies', (req, res) => {
-    const myMovies = [
+    const myMovies = [                     // Array containing movie objects
         {
             "Title": "Avengers: Infinity War",
             "Year": "2018",
@@ -47,29 +52,34 @@ app.get('/api/movies', (req, res) => {
         }
     ];
 
+    // Responding with the movie data in JSON format and setting status to 201 (Created)
     res.status(201).json({ myMovies });
 });
 
-
-// Route to serve index.html
+// Route to serve the index.html file
 app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html')); // Using __dirname for a safer path
+    res.sendFile(path.join(__dirname, 'index.html'));  // Sends the 'index.html' file using an absolute path
 });
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public'))); // Absolute path to 'public' directory
+// Middleware to serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));  // Static files (e.g., CSS, images) served from the 'public' folder
 
+// Route that accepts query parameters for the first and last name and responds with a hello fname lname
 app.get('/name', (req, res) => {
-    const firstname = req.query.firstname;
-    const lastname = req.query.lastname;
+    const firstname = req.query.firstname;   // Extract 'firstname' from the query string
+    const lastname = req.query.lastname;     // Extract 'lastname' from the query string
     res.send(`Hello ${firstname} ${lastname}`);
 });
 
+// Importing body parser to parse form data
 const bodyParser = require('body-parser');
+
+// Middleware to parse URL-encoded form data 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Route that handles POST requests for the /name endpoint, extracting form data and sends back fname lname message
 app.post('/name', (req, res) => {
-    const firstname = req.body.firstname;
-    const lastname = req.body.lastname;
+    const firstname = req.body.firstname;  // Extract 'firstname' from the form body
+    const lastname = req.body.lastname;    // Extract 'lastname' from the form body
     res.send(`Hello ${firstname} ${lastname}`);
 });
