c1eb095412d2bc1d924127acf9ef16a3b92869ff
unknown <g00391599@atu.ie>
2024-10-21 13:59:53 +0000

    Ex8

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path'); // Importing the 'path' module
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('wompy wompy womp');
+    res.send('Welcome to data rep and querying!!');
 });
 
 app.listen(port, () => {
@@ -19,4 +20,56 @@ app.get('/hello/:name/:surname', (req, res) => {
     const name = req.params.name;
     const surname = req.params.surname;
     res.send(`Hello ${name} ${surname}`);
+});
+
+app.get('/api/movies', (req, res) => {
+    const myMovies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+
+    res.status(201).json({ myMovies });
+});
+
+
+// Route to serve index.html
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html')); // Using __dirname for a safer path
+});
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public'))); // Absolute path to 'public' directory
+
+app.get('/name', (req, res) => {
+    const firstname = req.query.firstname;
+    const lastname = req.query.lastname;
+    res.send(`Hello ${firstname} ${lastname}`);
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/name', (req, res) => {
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    res.send(`Hello ${firstname} ${lastname}`);
 });
